20c645391c8058571eb7917725ebd3170c00ccd1
Abraham R <abraham_rq@outlook.com>
2025-11-12 13:04:13 -0600

    first three funtctions done

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 export function mayorDeTresNumeros(a, b, c) {
-  
+	return Math.max(a, b, c);  
 }
 export function ceroIfNull(a) {
- 
+	return a ?? 0; 
 }
 
 export function numerosParesCount(desde, hasta) {
-
+	let count = 0;
+	for (let i = desde; i <= hasta; i++) {
+        	if (i % 2 === 0) {
+            		count++;
+        	}
+    	}
+    	return count;
 }
 
 export function ifCadenaMasLargaDeN(cadena, n) {
